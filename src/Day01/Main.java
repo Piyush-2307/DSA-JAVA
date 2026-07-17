ef96fd38d862879a5bc152c2edc0be88242043b6
Peeyush Arora <peeyusharora831@gmail.com>
2026-07-17 13:16:53 +0530

    First commit

---
+++ src/Day01/Main.java
@@ -0,0 +1,10 @@
+package Day01;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!" + "Peeyush is here YOO!!!");
+        System.out.print("Google ");
+        System.out.println("target");
+        System.out.print(4 + 10);
+    }
+}
