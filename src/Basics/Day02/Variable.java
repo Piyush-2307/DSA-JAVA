bb131da75ad4fcc3b361f0b7ff9c9325aa324416
Peeyush Arora <peeyusharora831@gmail.com>
2026-07-18 23:47:55 +0530

    Second commit

---
+++ src/Basics/Day02/Variable.java
@@ -0,0 +1,10 @@
+package Basics.Day02;
+
+public class Variable {
+    public static void main(String[] args){
+        int a = 55;
+        int b = a;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
