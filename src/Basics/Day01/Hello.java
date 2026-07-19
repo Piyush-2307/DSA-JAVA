b2356c368fae9c72c4dae7bb58cda8c3443b3f55
Peeyush Arora <peeyusharora831@gmail.com>
2026-07-19 14:50:05 +0530

    third commit

---
+++ src/Basics/Day01/Hello.java
@@ -0,0 +1,10 @@
+package Basics.Day01;
+
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World!" + "Peeyush is here YOO!!!");
+        System.out.print("Google ");
+        System.out.println("target");
+        System.out.print(4 + 10);
+    }
+}
