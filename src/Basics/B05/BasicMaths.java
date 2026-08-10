f16211bbdd2844de65ef97e91a60e38d53e80070
Peeyush Arora <peeyusharora831@gmail.com>
2026-08-10 22:22:53 +0530

    OOP's theory

--- src/Basics/B05/BasicMaths.java
+++ src/Basics/B05/BasicMaths.java
@@ -1,5 +1,7 @@
 package Basics.B05;
 
+import java.util.Scanner;
+
 public class BasicMaths {
     static int printAllNum(int num){
         while (num != 0){
@@ -42,15 +44,82 @@ public class BasicMaths {
         return revNum;
     }
 
+    static boolean PrimeNum(int primeIp){
+        if (primeIp < 1){
+            System.out.println(primeIp + " num is less than 1");
+            return false;
+        }else {
+            for (int i = 2; i * i <= primeIp; i++) { //O(n) time complexity
+                if (primeIp % i == 0){
+                    System.out.println(primeIp + " it is not a prime num");
+                    return false;
+                }
+            }
+        }
+        System.out.println(primeIp + " it is a prime num");
+        return true;
+    }
+
+    static int getGCD(int a, int b){
+        while (b != 0){
+            int oldVal = b;
+            b = a % b;
+            a = oldVal;
+        }
+        return a;
+    }
+
+    static int getLCM(int a, int b){
+        int gcd = getGCD(a,b);
+        int prod = a * b;
+        int lcm = prod / gcd;
+        return lcm;
+    }
+
+    static int getArm(int arms){
+        int val = 0;
+        int ogNum = arms;
+        while (arms != 0){
+            int digit = arms % 10;
+            int cubeOfDigit = digit * digit * digit;
+//            System.out.println(cubeOfDigit);
+            val = val + cubeOfDigit;
+            arms = arms / 10;
+        }
+        if (val == ogNum){
+            System.out.println(val + " it is a armstrong num");
+        }else {
+            System.out.println(val + " it is not a armstrong num");
+        }
+        return val;
+    }
+
+
     public static void main(String[] args) {
-        int num = 8762;
-        int revNum = reverseNum(num);
-        System.out.println("Reverse num is: " + revNum);
+        Scanner sc = new Scanner(System.in);
+//        int arms = sc.nextInt();
+//        getArm(arms);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(getGCD(a,b));
+//        System.out.println(getLCM(a,b));
+//        int num = 8762;
+//        int primeIp = sc.nextInt();
+//        System.out.println(PrimeNum(primeIp));
+//        int revNum = reverseNum(num);
+//        System.out.println("Reverse num is: " + revNum);
+//        if (num == revNum){
+//            System.out.println(num + ", " + revNum + " is palindrome");
+//        }else {
+//            System.out.println(num + ", " + revNum + " is not palindrome");
+//        }
 //        int total = sumAllNum(num);
 //        System.out.println("Sum of the num is: " + total);
 //        int ans =countAllNum(num);
 //        System.out.println("total digits are: " + ans);
-        printAllNum(num);
+//        printAllNum(num);
+
+
     }
 
 }
---
+++ src/OOPS/OB01.java
@@ -0,0 +1,6 @@
+package OOPS;
+
+public class OB01 {
+
+    //today is the theory part
+}
